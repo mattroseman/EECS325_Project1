d642c84e28e37c1d62c80c7550a31baea6bb4eb3
Matthew Roseman <mrr77@case.edu>
2016-03-17 16:54:09 -0400

    changed names to project appropriate names

---
+++ src/proxyd.java
@@ -0,0 +1,18 @@
+package WebProxy;
+
+import java.net.*;
+import java.io.*;
+
+/* 
+*  socket to use = 5000 + 26 = 5026
+*  must hand GET and POST requests
+*  no pipelining but need to have persistent connection
+*  have to handle multiple parallel connections
+*/
+
+public class ProxyD {
+
+    public static void main(String[] args) {
+
+    }
+}
